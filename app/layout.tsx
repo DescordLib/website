acd6cd6fae6f42e0bd3be1a5f9323f457c1703ab
FirePlank <44502537+FirePlank@users.noreply.github.com>
2024-07-23 15:51:38 +0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="w-full h-full">
+      <head>
+        <title>Descord</title>
+        <meta name="description" content="Descord is a minimal Rust library that provides a simple and efficient way to interact with the Discord API." />
+        <meta property="og:title" content="Descord" />
+        <meta property="og:description" content="Descord is a minimal Rust library that provides a simple and efficient way to interact with the Discord API." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://descord.vercel.app" />
+        <meta property="og:image" content="/images/logo.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+      </head>
       <body className="justify-center items-center">
         {children}
       </body>
